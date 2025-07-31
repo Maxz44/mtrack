4d2802cdaa60599e73d2f959223cf3c0112db4a3
Maxz44 <Maxz44@users.noreply.github.com>
2025-07-31 15:39:13 +0200

    Filter using search keyword

--- src/lib/MangaList.svelte
+++ src/lib/MangaList.svelte
@@ -4,11 +4,15 @@
 
 <div class="flex two">
     {#each $mangas as manga}
-        <article class="card">
-            <header>
-                <h3>{manga}</h3>
-            </header>
-        </article>
+        {#if $search == "" || manga
+                .toLowerCase()
+                .includes($search.toLowerCase())}
+            <article class="card">
+                <header>
+                    <h3>{manga}</h3>
+                </header>
+            </article>
+        {/if}
     {/each}
 </div>
 
