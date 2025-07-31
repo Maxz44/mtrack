3d929b26fde82dd48b8c422a407ee5a45aa74aec
Maxz44 <Maxz44@users.noreply.github.com>
2025-07-31 09:47:50 +0200

    Create list of manga

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import MangaList from "./lib/MangaList.svelte";
     import MangaSearch from "./lib/MangaSearch.svelte";
+
     let search: string = $state("");
     let mangas: Array<string> = $state([]);
 </script>
 
 <div>
     <MangaSearch {search} {mangas} />
+    <MangaList {search} {mangas} />
 </div>
 
 <style>
---
+++ src/lib/MangaList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let { search, mangas }: { search: string; mangas: Array<string> } =
+        $props();
+</script>
+
+<div class="flex two">
+    {#each mangas as manga}
+        <article class="card">
+            <header>
+                <h3>{manga}</h3>
+            </header>
+        </article>
+    {/each}
+</div>
+
+<style></style>
