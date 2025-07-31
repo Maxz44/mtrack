ca126a1f2f77ff8d605e2f5ad09a50c4f43c75b1
Maxz44 <Maxz44@users.noreply.github.com>
2025-07-31 09:30:03 +0200

    Add MangaSearch component

---
+++ src/lib/MangaSearch.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let search: string = $state("");
+    let mangas: Array<string> = $state([]);
+
+    function add(e: Event) {
+        e.preventDefault();
+        mangas.push(search);
+        search = "";
+    }
+
+    function can_add() {
+        return search.length > 0 && !mangas.includes(search);
+    }
+</script>
+
+<form onsubmit={add}>
+    <input placeholder="Search your manga" bind:value={search} />
+    {#if can_add()}
+        <button class="full">Add</button>
+    {/if}
+</form>
+
+<style></style>
